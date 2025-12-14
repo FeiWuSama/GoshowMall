9230385e14c062cfff376dda50cb7f6c15b2fdf7
FeiWuSama <1825578184@qq.com>
2025-12-14 18:11:35 +0800

    fix-修改返回用户vo对象的json名称

--- adaptor/repo/vo/user.go
+++ adaptor/repo/vo/user.go
@@ -2,7 +2,7 @@ package vo
 
 type UserVo struct {
 	Id       int64  `json:"id"`
-	Nickname string `json:"Nickname"`
+	Nickname string `json:"nickname"`
 	Sex      int32  `json:"sex"`
 	Avatar   string `json:"avatar"`
 	Token    string `json:"token"`
--- router/white_list.go
+++ router/white_list.go
@@ -4,6 +4,7 @@ var whiteList = map[string]bool{
 	"/health":                     true,
 	"/check":                      true,
 	"/admin/captcha/slide":        true,
+	"/user/info":                  true,
 	"/user/captcha/slide":         true,
 	"/user/captcha/slide/verify":  true,
 	"/user/mobile/login/password": true,
